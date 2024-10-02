43a05f95f7d9853590abc5c4be000d987f2d22ee
jisan <jalal.uddin@tigerit.com>
2024-10-02 18:33:47 +0600

    Added methods in  GoalRepository

--- src/main/java/com/example/app/tasktracker/repository/GoalRepository.java
+++ src/main/java/com/example/app/tasktracker/repository/GoalRepository.java
@@ -1,4 +1,13 @@
 package com.example.app.tasktracker.repository;
 
+import com.example.app.tasktracker.model.Goal;
+
+import java.util.List;
+
 public interface GoalRepository {
+    Goal create(Goal goal);
+    Goal update(Goal goal);
+    Goal findById(Long id);
+    List<Goal> findAll();
+    void deleteById(Long id);
 }
